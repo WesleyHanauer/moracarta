86cdd2427f83b1da16d5a175953956fc3c490287
WesleyHanauer <wesleyhanauer@gmail.com>
2026-09-02 14:11:54 -0300

    refactor: rename all style variables to english

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -62,9 +62,9 @@ function renderLetters() {
 
     link.innerHTML = `
       <div class="envelope-wrap">
-        <div class="envelope-sombra"></div>
-        <div class="envelope" style="background:${palette.envelope}; --envelope-sombra:${palette.shadow};">
-          <div class="env-corpo-svg">
+        <div class="envelope-shadow"></div>
+        <div class="envelope" style="background:${palette.envelope}; --envelope-shadow:${palette.shadow};">
+          <div class="env-body-svg">
             <svg viewBox="0 0 100 63" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
               <polygon points="0,0 0,63 50,31" fill="${palette.shadow}" />
               <polygon points="100,0 100,63 50,31" fill="${palette.shadow}" />
@@ -72,22 +72,22 @@ function renderLetters() {
             </svg>
           </div>
 
-          <div class="env-aba" style="border-top-color:${palette.flap};">
+          <div class="env-flap" style="border-top-color:${palette.flap};">
             <svg viewBox="0 0 100 44" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
               <polygon points="0,0 100,0 50,44" fill="${palette.flap}" />
             </svg>
-            <div class="env-lacre" style="background:${palette.seal};">♡</div>
+            <div class="env-seal" style="background:${palette.seal};">♡</div>
           </div>
 
-          <div class="envelope-centro">
-            <span class="envelope-data" style="color:${palette.text};">${letter.date}</span>
-            <span class="envelope-titulo" style="color:${palette.text};">${letter.title}</span>
+          <div class="envelope-center">
+            <span class="envelope-date" style="color:${palette.text};">${letter.date}</span>
+            <span class="envelope-title" style="color:${palette.text};">${letter.title}</span>
             <span class="envelope-hint" style="color:${palette.text};">Tap to open</span>
           </div>
-          <div class="envelope-canto tl"></div>
-          <div class="envelope-canto tr"></div>
-          <div class="envelope-canto bl"></div>
-          <div class="envelope-canto br"></div>
+          <div class="envelope-corner tl"></div>
+          <div class="envelope-corner tr"></div>
+          <div class="envelope-corner bl"></div>
+          <div class="envelope-corner br"></div>
         </div>
       </div>
     `;
